ce469184b32bc2f5777621dbf9502746ab588909
AppleMango23 <ninjayek@gmail.com>
2024-07-20 15:45:13 +0800

    Show out render floating input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -169,7 +169,7 @@ export default function TabOneScreen() {
           }
         />
       </View>
-      {/* {renderFloatingInput()} */}
+      {renderFloatingInput()}
     </KeyboardAwareScrollView>
   );
 }
